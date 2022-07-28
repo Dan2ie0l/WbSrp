9fc6189105a6365924d5065f3bc84d13161ba8be
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2022-07-28 19:22:19 +0400

    creating database layer

---
+++ WebPScrapping/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebPScrapping
+{
+    public class AppContext:DbContext
+    {
+        public DbSet<PornstarModel> Users => Set<PornstarModel>();
+        public AppContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=(localdb)\ProjectModels;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ WebPScrapping/Models/Images.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPScrapping.Models
+{
+    internal class Images
+    {
+        public int Id { get; set; }
+        public List<string> Urls { get; set; }
+    }
+}
